eaf7b2df9fd339b2650b3ff82676bf9dda90240f
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2018-12-27 17:33:57 -0300

    testando modelos preditivos com o sklearn

--- sklean/ClassifyNB.py
+++ sklean/ClassifyNB.py
@@ -7,6 +7,4 @@ def classify(features_train, labels_train):
     ### your code goes here!
     from sklearn.naive_bayes import GaussianNB
     clf = GaussianNB()
-    clf.fit(features_train, labels_train)
-    pred = clf.predict(features_test)
-    return pred
+    return clf.fit(features_train, labels_train)
